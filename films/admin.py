c196a250d2d035b6898d822924f7e7d470ee3d14
KairatSultankulov <sultankulov.kairat@gmail.com>
2025-04-11 15:02:09 +0600

    l2

--- films/admin.py
+++ films/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from films.models import Film
+from films.models import Film, Director, Genre, Review
 
-admin.site.register(Film)
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+
+class FilmAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
+    search_fields = ['name']
+    list_filter = ['genres', 'director']
+    list_display = ['name', 'director', 'is_active', 'created']
+
+admin.site.register(Film, FilmAdmin)
+admin.site.register(Director)
+admin.site.register(Genre)
--- films/serializers.py
+++ films/serializers.py
@@ -1,5 +1,17 @@
 from rest_framework import serializers
-from .models import Film
+from .models import Film, Director, Genre
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = 'id', 'fio'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
 
 class FilmDetailSerializer(serializers.ModelSerializer):
@@ -9,7 +21,14 @@ class FilmDetailSerializer(serializers.ModelSerializer):
 
 
 class FilmSerializer(serializers.ModelSerializer):
+    director = DirectorSerializer()
+    genres = GenreSerializer(many=True)
+    director_fio = serializers.SerializerMethodField()
+
     class Meta:
         model = Film
         #fields = ['id', 'name', 'kp_rating', 'created']
-        fields = 'id name kp_rating text'.split()
+        fields = 'id name kp_rating text director genres director_fio genre_names'.split()
+
+    def get_director_fio(self, film):
+        return film.director.fio if film.director_id else None
